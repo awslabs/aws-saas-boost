458cf79f9f1a224589f9ab4d01527052362461f3
PoeppingT <poeppt@amazon.com>
2023-03-01 12:38:38 -0800

    Fix Metric Line Graphs, fix Sysuser create redirect.
    This commit fixes two small bugs:
    1. The previous fix for fixing the metrics line graphs was incomplete.
       This commit fixes it for good by using the Date calculation if it can
       be successfully converted into a Date object, and just using the
       returned Date otherwise.
    2. When we moved to calling things "system-user-service", the redirect
       from the UserCreateContainer was missed, resulting in a broken
       breadcrumb when creating a user. Changing the redirect location fixes
       this issue.

--- client/web/src/metrics/ducks/index.js
+++ client/web/src/metrics/ducks/index.js
@@ -62,10 +62,10 @@ const metricQueryAdapter = createEntityAdapter({
 const conditionDates = (response) => {
   const formatDate = (date) => {
     const d = new Date(`${date}`)
-    return `${d.getMonth() + 1}-${d.getDate()} ${d.getHours().toString().padStart(2, '0')}:${d
+    return !!d ? `${d.getMonth() + 1}-${d.getDate()} ${d.getHours().toString().padStart(2, '0')}:${d
       .getMinutes()
       .toString()
-      .padStart(2, '0')}`
+      .padStart(2, '0')}` : date
   }
   const metrics = response[0] || {}
   const fixedDates = {
