e4e13fe93a7300576e064eaaf9785507b6fdddd2
PoeppingT <PoeppingT@users.noreply.github.com>
2023-05-31 16:35:50 -0700

    Include yarn.lock in installer client zipfile. (#517)
    Co-authored-by: PoeppingT <poeppt@amazon.com>

--- installer/src/main/java/com/amazon/aws/partners/saasfactory/saasboost/SaaSBoostInstall.java
+++ installer/src/main/java/com/amazon/aws/partners/saasfactory/saasboost/SaaSBoostInstall.java
@@ -1851,6 +1851,7 @@ public class SaaSBoostInstall {
                     .filter(file ->
                             Files.isRegularFile(file) && (
                             file.startsWith("client/web/package.json")
+                            || file.startsWith("client/web/yarn.lock")
                             || file.startsWith("client/web/.npmrc")
                             || file.startsWith("client/web/public")
                             || file.startsWith("client/web/src"))
